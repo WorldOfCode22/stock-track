cf868e6e1565d4f6a1e2f1515995b7457ce8a0c4
Sloan <gwaltneys2@winthrop.edu>
2018-02-27 01:30:35 -0500

    added mongoose model

--- api.js
+++ api.js
@@ -1,6 +1,9 @@
+// App setup
 const express = require('express');
 const app = express();
 const port = 4000;
+
+//mongoose setup
 
 app.listen(port,()=>{
   console.log(`API waiting for request on port ${port}`);
