466c1008e12ec9c4c1d7cd4bd4b48baabb91771c
Sloan <gwaltneys2@winthrop.edu>
2018-02-27 11:52:47 -0500

    Removed routes for now. Going to focus on a websocket approach

--- api.js
+++ api.js
@@ -16,48 +16,7 @@ let stocks = new mongoose.Schema({
 })
 let stockModel = mongoose.model("currentStock", stocks);
 // route setup
-app.get('/stocks/add/:stock',(req,res)=>{
-  stockModel.count()
-  .then((count)=>{
-    if(count === 1){
-      stockModel.findOne()
-      .then((doc)=>{
-        doc.currentStocks.push(req.params.stock);
-        return doc.save();
-      }).then((doc)=>{
-        return fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${req.params.stock}&apikey=${process.env.STOCK_API}`)
-        .then((data)=>{
-          res.json(data);
-        })
-      }).catch((err)=>{res.json({error:err})})
-    }else if(count === 0){
-      new stockModel({
-        currentStocks: [req.params.stock]
-      }).save.then((doc)=>{
-        res.json(doc);
-      })
-    }else{
-      res.json({message: "Database Error"});
-    }
-  })
-})
-app.get('/stocks',(req,res)=>{
-  stockModel.count()
-  .then((count)=>{
-    if(count === 1){
-      stockModel.findOne().then((doc)=>{
-        res.json({currentStocks: doc.currentStocks});
-      })
-    }else if (count === 0){
-      new stockModel({}).save.then(()=>{
-        res.json({message: "No Stocks Active"});
-      })
-    }else{
-      console.log("Too Many Collections In DB");
-      res.json({error: "Database Error"});
-    }
-  })
-});
+
 // app deploy
 app.listen(port,()=>{
   console.log(`API waiting for request on port ${port}`);
