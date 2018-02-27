1994857456c09e67500c439dd1ba5f5a0f96d532
Sloan <gwaltneys2@winthrop.edu>
2018-02-27 01:42:45 -0500

    fixed mongoose error. Added mongoose conection. Added end point

--- api.js
+++ api.js
@@ -6,10 +6,19 @@ const port = 4000;
 const mongoose = require('mongoose');
 
 //mongoose setup
+mongoose.connect(process.env.MONGO_URI)
+.then(
+  () => {console.log("Datbase Connected")},
+  err =>{console.log(`Error connecting to database ${err}`)}
+)
 let stocks = new mongoose.Schema({
-  currentStocks = [];
+  currentStocks:Array
 })
 let stockModel = mongoose.model("currentStock", stocks);
+// route setup
+app.get('/stocks',()=>{
+
+});
 // app deploy
 app.listen(port,()=>{
   console.log(`API waiting for request on port ${port}`);
