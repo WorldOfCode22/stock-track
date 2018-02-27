ce0e00136da62ba74250ac9daf8c5d8f2d68c3b4
Sloan <gwaltneys2@winthrop.edu>
2018-02-27 02:35:16 -0500

    Added start of 2 endpoints for stock get/add

--- api.js
+++ api.js
@@ -16,8 +16,45 @@ let stocks = new mongoose.Schema({
 })
 let stockModel = mongoose.model("currentStock", stocks);
 // route setup
-app.get('/stocks',()=>{
-
+app.get('/stocks/add/:stock',(req,res)=>{
+  stockModel.count()
+  .then((count)=>{
+    if(count === 1){
+      stockModel.findOne()
+      .then((doc)=>{
+        doc.currentStocks.push(req.params.stock);
+        return doc.save();
+      }).then((doc)=>{
+        res.json(doc);
+        return doc;
+      }).catch((err)=>{res.json({error:err})})
+    }else if(count === 0){
+      new stockModel({
+        currentStocks: [req.params.stock]
+      }).save.then((doc)=>{
+        res.json(doc);
+      })
+    }else{
+      res.json({message: "Database Error"});
+    }
+  })
+})
+app.get('/stocks',(req,res)=>{
+  stockModel.count()
+  .then((count)=>{
+    if(count === 1){
+      stockModel.findOne().then((doc)=>{
+        res.json({currentStocks: doc.currentStocks});
+      })
+    }else if (count === 0){
+      new stockModel({}).save.then(()=>{
+        res.json({message: "No Stocks Active"});
+      })
+    }else{
+      console.log("Too Many Collections In DB");
+      res.json({error: "Database Error"});
+    }
+  })
 });
 // app deploy
 app.listen(port,()=>{
