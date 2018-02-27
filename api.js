2286c1f63021281f08eeb21a39e0d14d5d71e79e
Sloan <gwaltneys2@winthrop.edu>
2018-02-27 01:31:05 -0500

    added mongoose model

--- api.js
+++ api.js
@@ -1,10 +1,16 @@
 // App setup
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 4000;
+const mongoose = require('mongoose');
 
 //mongoose setup
-
+let stocks = new mongoose.Schema({
+  currentStocks = [];
+})
+let stockModel = mongoose.model("currentStock", stocks);
+// app deploy
 app.listen(port,()=>{
   console.log(`API waiting for request on port ${port}`);
 })
