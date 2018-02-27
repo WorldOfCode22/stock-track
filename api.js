7fbc51d723137992f9c21145ce623a2d94c06c18
Sloan <gwaltneys2@winthrop.edu>
2018-02-27 13:39:56 -0500

    added create react app and socket connection log

--- api.js
+++ api.js
@@ -2,9 +2,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
 const port = 4000;
 const mongoose = require('mongoose');
 const fetch = require('node-fetch');
+const io = require('socket.io')(http);
 //mongoose setup
 mongoose.connect(process.env.MONGO_URI)
 .then(
@@ -15,9 +17,14 @@ let stocks = new mongoose.Schema({
   currentStocks:Array
 })
 let stockModel = mongoose.model("currentStock", stocks);
-// route setup
+// Socket setup
+io.on('connection',()=>{
+  io.on('add stock', (stock)=>{
+    console.log(stock);
+  })
+})
 
 // app deploy
-app.listen(port,()=>{
+http.listen(port,()=>{
   console.log(`API waiting for request on port ${port}`);
 })
