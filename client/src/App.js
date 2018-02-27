88471246122d1e2b3b54fa9fdb45250fb2e863cc
Sloan <gwaltneys2@winthrop.edu>
2018-02-27 13:52:55 -0500

    Added basic socket connection between client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import openSocket from 'socket.io-client';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={socket:openSocket('http://localhost:4000')};
+  }
   render() {
     return (
       <div className="App">
