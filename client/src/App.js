358601611f1ce8b912afea615638df18a4fd18e9
Sloan <gwaltneys2@winthrop.edu>
2018-02-27 23:43:51 -0500

    Rendering of stock card with stock info

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import openSocket from 'socket.io-client';
-
+import StockCard from './components/stock-card';
 class App extends Component {
   constructor(){
     super();
     let socket = openSocket('http://localhost:4000');
     socket.on("get current stocks", (stocks)=>{
-      console.log(stocks);
+      this.jsxifyStocks(stocks);
     })
     this.state={socket};
+  }
+  jsxifyStocks(stockArr){
+    let jsxArray = []
+    for(let i = 0; i<stockArr.length;i++){
+      jsxArray.push(<StockCard key={i} stock={stockArr[i]} />);
+    }
+    this.setState({stocks:jsxArray});
   }
   render() {
     return (
       <div className="App">
-
+        {this.state.stocks}
       </div>
     );
   }
--- client/src/components/stock-card.js
+++ client/src/components/stock-card.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 
 export default class StockCard extends Component{
   render(){
-    divStyle = {
-      "backgroundColor": this.props.color
+    console.log(this.props);
+    const divStyle = {
+      backgroundColor: this.props.color,
+      textAlign: "center"
     }
     return(
       <div style={divStyle}>
+        <h1>{this.props.stock["Meta Data"]["2. Symbol"]}</h1>
+        <button>remove</button>
       </div>
     )
   }
