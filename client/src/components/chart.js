59c3319f823657adb2d90bcb9451693c760e7d20
Sloan <gwaltneys2@winthrop.edu>
2018-03-01 12:01:26 -0500

    Basic dev MVP

--- client/src/components/chart.js
+++ client/src/components/chart.js
@@ -3,33 +3,36 @@ import Graph from 'chart.js';
 export default class Chart extends Component{
   formatData(){
     let self = this;
-    if(this.props.stocks){
-    let objArray = []
-    for(let i = 0; i < this.props.stocks.length; i++){
-      let obj = {x:[], y:[]};
-      let keys = Object.keys(this.props.stocks[i]["Time Series (Daily)"]);
-      console.log(this.props.stocks[i]["Time Series (Daily)"]);
-      keys.forEach(function keyFilter(e, z){
-        obj.x.push(keys[z]);
-        obj.y.push(self.props.stocks[i]["Time Series (Daily)"][keys[z]]["2. high"])
-      })
-      obj.name = this.props.stocks[i]["Meta Data"]["2. Symbol"];
-      objArray.push(obj);
+    let obj = {};
+    let keys = Object.keys(this.props.stocks[0]["Time Series (Daily)"])
+    function getLabels(){
+      return keys;
     }
-    console.log(objArray);
-    return objArray;
-  }
+    function formatDatasets(){
+      let dataset = [];
+      let dataObj = {};
+      for(let i = 0; i < self.props.stocks.length; i++){
+        dataObj.label = self.props.stocks[i]["Meta Data"]["2. Symbol"];
+        dataObj.data = [];
+        keys.forEach(function getVals(c){
+          dataObj.data.push(self.props.stocks[i]["Time Series (Daily)"][c]["2. high"])
+        })
+        dataset.push(dataObj);
+        dataObj = {};
+      }
+      return dataset;
+    }
+    obj.labels = keys;
+    obj.datasets = formatDatasets();
+    return obj;
 }
   setupChart(){
     console.log(this.formatData())
     let lineChart = new Graph(document.getElementById('chart'),{
       type: 'line',
       data: {
-        labels: this.formatData()[0].x,
-        datasets: [{
-          data: this.formatData()[0].y,
-          label: this.formatData()[0].name
-        }]
+        labels: this.formatData().labels,
+        datasets: this.formatData().datasets
       }
     })
   }
